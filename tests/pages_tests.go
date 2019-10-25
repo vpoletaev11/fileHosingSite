f1c3b3f6543d0010d8300842ef3a9234a70f88f9
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2019-10-25 15:02:11 -0400

    add vendoring

--- tests/pages_tests.go
+++ tests/pages_tests.go
@@ -2,12 +2,10 @@ package tests
 
 import (
 	"testing"
-
-	"github.com/vpoletaev11/fileHostingSite/pages/login"
 )
 
 // TestLoginPage tests login page (your cap)
 func TestLoginPage(t *testing.T) {
-	url := "http://127.0.0.1"
-	handler := login.Page(db)
+	// url := "http://127.0.0.1"
+	// handler := login.Page(db)
 }

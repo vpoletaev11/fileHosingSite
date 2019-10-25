0c7e4769159c5e4edd34bc93de269370422dfa68
vpoletaev11 <vpoletaev11@gmail.com>
2019-10-25 17:27:13 +0300

    AuthWrapper now extends cookie lifetime instead of  creating new cookie

--- cookie/cookie.go
+++ cookie/cookie.go
@@ -37,17 +37,19 @@ func CreateCookie(username string) (http.Cookie, error) {
 	return cookie, nil
 }
 
-// cookieValidator handling cases when cookie:
-// 1) don't came on input,
-// 2) out of date,
-// 3) aren't contains in database.
+// cookieValidator returns username and cookie when cookie:
+// 1) came on input,
+// 2) doesn't out of date,
+// 3) contains in database.
 func cookieValidator(db *sql.DB, r *http.Request) (string, http.Cookie) {
+	// handling case when cookie doesn't came to input
 	cookie, err := r.Cookie("session_id")
 	if err != nil {
 		emptyCookie := &http.Cookie{}
 		return "", *emptyCookie
 	}
 
+	// handling case when cookie is out of date
 	if cookie.Expires.After(time.Now()) {
 		_, err = db.Exec(deleteCookie, cookie.Value)
 		if err != nil {
@@ -57,11 +59,10 @@ func cookieValidator(db *sql.DB, r *http.Request) (string, http.Cookie) {
 		return "", *emptyCookie
 	}
 
+	// handling case when cookie aren't contains in database
 	username := ""
 	err = db.QueryRow(userFromCookie, cookie.Value).Scan(&username)
-
 	if err != nil {
-		// fmt.Println(err)
 		emptyCookie := &http.Cookie{}
 		return "", *emptyCookie
 	}
@@ -69,24 +70,20 @@ func cookieValidator(db *sql.DB, r *http.Request) (string, http.Cookie) {
 	return username, *cookie
 }
 
-// AuthWrapper afsasdf
+// AuthWrapper grants access to pagehandler and extends cookie lifetime if inputed cookie are valid
 func AuthWrapper(pageHandler http.Handler, db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		username, oldCookie := cookieValidator(db, r)
+		// checking cookie validity
+		username, cookie := cookieValidator(db, r)
 		if username == "" {
 			http.Redirect(w, r, "/login", http.StatusFound)
 			return
 		}
 
-		cookie, err := CreateCookie(username)
-
-		db.Exec(updateCookie, cookie.Value, oldCookie.Value)
-
-		if err != nil {
-			fmt.Println(w, err)
-		}
-
+		// extending cookie lifetime
+		cookie.Expires = time.Now().Add(30 * time.Minute)
 		http.SetCookie(w, &cookie)
+
 		// run page handler
 		pageHandler.ServeHTTP(w, r)
 	})

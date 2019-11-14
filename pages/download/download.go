9526fd5fce563143590db0771e55f0af44388eb8
vpoletaev11 <vpoletaev11@gmail.com>
2019-11-14 23:39:05 +0300

    Download page now displays file information.

--- pages/download/download.go
+++ pages/download/download.go
@@ -5,15 +5,30 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 // absolute path to template file
 const absPathTemplate = "/home/perdator/go/src/github.com/vpoletaev11/fileHostingSite/templates/download.html"
 
+const fileInfoDB = "SELECT * FROM files WHERE id = ?;"
+
 // TemplateDownload contains fields with warning message and username for login page handler template
 type TemplateDownload struct {
 	Warning  template.HTML
 	Username string
+	FileInfo
+}
+
+type FileInfo struct {
+	ID          int
+	Label       string
+	FilesizeMB  string
+	Description string
+	Owner       string
+	Category    string
+	UploadDate  string
+	Rating      int
 }
 
 // Page returns HandleFunc with access to MySQL database for download page
@@ -28,7 +43,30 @@ func Page(db *sql.DB, username string) http.HandlerFunc {
 		}
 		switch r.Method {
 		case "GET":
-			page.Execute(w, TemplateDownload{Username: username})
+			fileID := r.URL.Query().Get("id")
+
+			fi := FileInfo{}
+
+			var uploadDateTime time.Time
+			filesizeBytes := 0
+			err := db.QueryRow(fileInfoDB, fileID).Scan(
+				&fi.ID,
+				&fi.Label,
+				&filesizeBytes,
+				&fi.Description,
+				&fi.Owner,
+				&fi.Category,
+				&uploadDateTime,
+				&fi.Rating,
+			)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fi.UploadDate = uploadDateTime.Format("2006-01-02 15:04:05")
+			//fi.FilesizeMB = filesizeBytes / 1024 / 1024
+			fi.FilesizeMB = fmt.Sprintf("%.4f", float64(filesizeBytes)/1024/1024) + " MB"
+
+			page.Execute(w, TemplateDownload{Username: username, FileInfo: fi})
 			return
 		}
 	}

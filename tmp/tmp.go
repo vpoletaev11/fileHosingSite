95fadbcabf3964d59071c3901ab0634c4b196964
vpoletaev11 <vpoletaev11@gmail.com>
2019-12-03 00:20:49 +0300

    CreateTemplate() rewritted

--- tmp/tmp.go
+++ tmp/tmp.go
@@ -8,34 +8,18 @@ import (
 
 // CreateTemplate creates template from inputted template file path
 func CreateTemplate(path string) (*template.Template, error) {
-	//
-	for i := 0; i < 10; i++ {
-		// getting current working directory
-		abspath, err := filepath.Abs("")
-		if err != nil {
-			return nil, err
-		}
-
-		switch {
-		// if working directory != root (when runned tests)
-		case filepath.Base(abspath) != "fileHostingSite":
-			err := os.Chdir("..")
-			if err != nil {
-				return nil, err
-			}
-
-		// if working directory == root (usual running of program)
-		case filepath.Base(abspath) == "fileHostingSite":
-			abspath = filepath.Join(abspath, path)
-			page, err := template.ParseFiles(abspath)
-			if err != nil {
-				return nil, err
-			}
-			return page, nil
-
-		default:
+	// if working directory != root - directory level will be lowered (case when used tests)
+	for i := 0; i < 5; i++ {
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			path = filepath.Join("../", path)
+		} else {
 			break
 		}
 	}
-	return nil, nil
+	// creating template from template file path
+	page, err := template.ParseFiles(path)
+	if err != nil {
+		return nil, err
+	}
+	return page, nil
 }

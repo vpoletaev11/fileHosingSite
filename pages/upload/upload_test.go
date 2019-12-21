3ec530355ae1b20b496bd470347e1fbde9b2af47
vpoletaev11 <vpoletaev11@gmail.com>
2019-12-21 22:02:32 +0300

    POST quires handler success case tested

--- pages/upload/upload_test.go
+++ pages/upload/upload_test.go
@@ -1,16 +1,33 @@
 package upload
 
 import (
+	"database/sql/driver"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+type anyTime struct{}
+
+// ()Match() checks is input value are time
+func (a anyTime) Match(v driver.Value) bool {
+	_, ok := v.(string)
+	if !ok {
+		return false
+	}
+	if len(v.(string)) != 19 {
+		return false
+	}
+	return ok
+}
 
 func TestPageSuccessGET(t *testing.T) {
 	sut := Page(nil, "username")
@@ -64,6 +81,101 @@ func TestPageSuccessGET(t *testing.T) {
 
             <p><input type="submit" value="UPLOAD"></p>
             
+        </form>
+        </div>
+    </div>
+</body>`, bodyString)
+}
+
+func TestPageSuccessPOST(t *testing.T) {
+	// changing directory because of test are not containing in root folder
+	os.Chdir("../../")
+	defer os.Chdir("pages/upload")
+
+	db, sqlMock, err := sqlmock.New()
+	require.NoError(t, err)
+	sqlMock.ExpectExec("INSERT INTO files").WithArgs(
+		"filename",
+		11,
+		"description",
+		"username",
+		"other",
+		anyTime{},
+	).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	postData :=
+		`--xxx
+Content-Disposition: form-data; name="filename"
+
+filename
+--xxx
+Content-Disposition: form-data; name="description"
+
+description
+--xxx
+Content-Disposition: form-data; name="category"
+
+other
+--xxx
+Content-Disposition: form-data; name="uploaded_file"; filename="file"
+Content-Type: application/octet-stream
+Content-Transfer-Encoding: binary
+
+binary data
+--xxx--
+`
+	r := &http.Request{
+		Method: "POST",
+		Header: http.Header{"Content-Type": {`multipart/form-data; boundary=xxx`}},
+		Body:   ioutil.NopCloser(strings.NewReader(postData)),
+	}
+
+	w := httptest.NewRecorder()
+
+	sut := Page(db, "username")
+	sut(w, r)
+
+	bodyBytes, err := ioutil.ReadAll(w.Body)
+	require.NoError(t, err)
+	bodyString := string(bodyBytes)
+	assert.Equal(t, `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Upload file</title>
+    <link rel="stylesheet" href="assets/css/upload.css">
+<head>
+<body bgcolor=#f1ded3>
+    <div class="menu">
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/categories">Categories</a></li>
+            <li><a href="/popular">Most popular</a></li>
+            <li><a href="/users">Users</a></li>
+            <li><a href="/logout">Logout</a></li>
+        </ul>
+    </div>
+    <div class="username">Welcome, username</div>
+
+    <div class="uploadFormBox">
+        <div class="uploadFormContent">
+        <form action="" method="post" enctype="multipart/form-data">
+            <p>Filename: <input type="text" maxlength="50" name="filename"></p><br>
+            <p>Input description for uploading file:</p>
+            <textarea cols="80" rows="15" maxlength="500" name="description"></textarea>
+    
+            <p>Category: <select name="category">
+                <option selected="selected" value="other">other</option>
+                <option value="games">games</option>
+                <option value="documents">documents</option>
+                <option value="projects">projects</option>
+                <option value="music">music</option>
+                </select></p>
+                   
+            <p><input required type="file" name="uploaded_file"></input></p>
+
+            <p><input type="submit" value="UPLOAD"></p>
+            <h2 style="color:green">FILE SUCCEEDED UPLOADED</h2>
         </form>
         </div>
     </div>
